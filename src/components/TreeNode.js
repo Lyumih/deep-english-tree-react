03ab7d2844e3894c856c3379ffd72f1b3993576f
Lyumih <32732179+Lyumih@users.noreply.github.com>
2018-09-13 06:58:23 +0300

    Add: treeNode and level config

---
+++ src/components/TreeNode.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import Node from './Node'
+
+
+const TreeNode = ({tree=[], topLevel=0,  level=1, downLevel=2}) => {
+    
+    
+    return(<div className="TreeNode">
+        <p>TreeNode English</p>
+        <Node 
+            rus={tree[topLevel].rus} 
+            eng={tree[topLevel].eng}/>
+        <Node 
+            rus={tree[level].rus} 
+            eng={tree[level].eng}/>
+        <Node 
+            rus={tree[downLevel].rus} 
+            eng={tree[downLevel].eng}/>
+    </div>
+)
+}
+
+export default TreeNode
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import './../App.css';
 
-import Node from './../components/Node'
+import TreeNode from './../components/TreeNode'
 
+const tree = [
+  {
+      "rus":"Еда",
+      "eng":"Eat",
+  },
+  {
+      "rus":"Фрукт",
+      "eng":"Fruct",
+  },
+  {
+      "rus":"Яблоко",
+      "eng":"Apple",
+  },
+  {
+      "rus":"Косточка",
+      "eng":"Stone",
+  }
+]
+
+
+const level = 2
+const topLevel = level - 1;
+const downLevel = level + 1;
 class App extends Component {
+
   render() {
+
     return (
       <div className="App">
-        <Node />
-        <Node rus="Фрукт" eng="fruct"/>
-        <Node rus="Яблоко" eng="apple"/>
+        <TreeNode 
+          tree={tree}
+          topLevel={topLevel}
+          level={level}
+          downLevel={downLevel}/>
       </div>
     );
   }
